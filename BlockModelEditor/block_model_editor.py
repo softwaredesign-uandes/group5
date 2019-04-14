02ddf0b296b587c3a5031164959b60ad5fce56d2
Martín Díaz <mfdiaz1@miuandes.cl>
2019-04-14 15:10:56 -0400

    Isolated Domain Model

--- BlockModelEditor/block_model_editor.py
+++ BlockModelEditor/block_model_editor.py
@@ -2,8 +2,8 @@ from pathlib import Path
 import block_model
 
 
-def import_block_model_from_zuck_file(filename):
-    new_blocks = []
+def import_block_model_from_file(filename, data_columns):
+    new_blocks = block_model.BlockModel()
     if Path(filename).is_file():
         source_file = open(filename, "r")
         file_lines = source_file.readlines()
@@ -12,9 +12,17 @@ def import_block_model_from_zuck_file(filename):
             
             block_values = line.split(' ')
 
-            #weight = float(block_values[0])
-            
-            new_blocks.append(block_values)
+            try:
+                position_x = int(block_values[data_columns[0]])
+                position_y = int(block_values[data_columns[1]])
+                position_z = int(block_values[data_columns[2]])
+                weight = float(block_values[data_columns[3]])
+                grade = 0
+                for i in range(4, len(data_columns)):
+                    grade += float(block_values[data_columns[i]])
+                new_blocks.add_block((position_x, position_y, position_z), block_model.Block(weight, grade))
+            except IndexError:
+                new_blocks = None
 
     return new_blocks
 
@@ -26,72 +34,81 @@ def get_user_input():
 
     return input("\nChoose a command: ")
 
+
 def get_user_query():
-    print("\nWhich information you want from the loaded model?")
+    print("\nWhich information do you want from the loaded model?")
     print("")
     print("[1] Number of blocks.")
     print("[2] Total weight of the Mineral Deposit.")
-    print("[3] Percentage of Air blocks (blocks with weight 0).")
+    print("[3] Percentage of Air blocks (blocks with weight of 0).")
     print("[4] Total mineral weight for all minerals in the Mineral Deposit")
     print("[5] Go Back.")
 
     return input("\nChoose a command: ")
 
-blocks = []
+
+def get_user_data_columns():
+    print("Please indicate in which column of the file the following variables are placed "
+          "(separated by spaces, 0-indexed)")
+    print("X POSITION | Y POSITION | Z POSITION | TOTAL WEIGHT | MINERAL 1 WEIGHT | MINERAL 2 WEIGHT | ...")
+
+    return input("\nInput: ")
+
+
+def import_block_model():
+    data_columns_input = get_user_data_columns()
+
+    if len(data_columns_input) < 5:
+        print("Error: Not enough column indexes inserted.")
+        return None
+
+    data_columns = [int(x) for x in data_columns_input.split()]
+
+    input_filename = input("Please insert the source file name: ")
+
+    imported_blocks = import_block_model_from_file(input_filename, data_columns)
+
+    if imported_blocks.get_total_block_number() == 0:
+        print("Error importing Block Model.")
+        return None
+    else:
+        print("Block Model imported successfully.")
+        return imported_blocks
+
+
+def query_block_model(current_block_model):
+    query = get_user_query()
+    while query is not '5':
+        if query is '1':
+            print("Number of blocks: " + str(current_block_model.get_total_block_number()))
+
+        elif query is '2':
+            print("Total weight of Block Model: " + str(current_block_model.get_total_weight()))
+
+        elif query is '3':
+            print("Percentage of air blocks in model: " + str(current_block_model.get_air_percentage()))
+
+        elif query is '4':
+            print("Total mineral weight of Block Model: " + str(current_block_model.get_total_grade()))
+
+        query = get_user_query()
+
+
+# Main
+blocks = block_model.BlockModel()
 
 user_input = get_user_input()
 
-while user_input is not 3:
+while user_input is not '3':
 
-    if user_input is 1:
-        print("Please indicate in which columns are the following data placed (spaced separated)")
-        print("TOTAL WEIGHT|MINERAL 1 WEIGHT|MINERAL 2 WEIGHT|...")
-        data = [int(x) for x in raw_input().split()]
-        data[:] = [x - 1 for x in data]
-        weight = data[0]
+    if user_input is '1':
+        new_block_model = import_block_model()
+        if new_block_model is not None:
+            blocks = new_block_model
 
-        input_filename = input("Please insert the source file name: ")
-
-        imported_blocks = import_block_model_from_zuck_file(input_filename)
-        
-        if len(imported_blocks) == 0:
-            print("Error importing Block Model.")
-        else:
-            blocks = imported_blocks
-            print("Block Model imported successfully.")
-
-
-    elif user_input is 2:
-        query = get_user_query()
-        while query is not 5:
-            if query is 1:
-                print(len(imported_blocks))
-                
-            elif query is 2:
-                Total_weight = 0
-                for i in imported_blocks:
-                    Total_weight += float(i[weight])
-                print(Total_weight)
-            elif query is 3:
-                zeros = 0
-                for i in imported_blocks:
-                    if i[weight] == 0:
-                        zeros += 1
-                if zeros == 0:
-                    print(zeros)
-                else:
-                    print(len(imported_blocks)/zeros)
-            elif query is 4:
-                Total_weight_minerals = 0
-                for i in imported_blocks:
-                    for j in data[0:]:
-                        Total_weight_minerals += float(i[j])
-                print(Total_weight_minerals) 
-
-            query = get_user_query()
-
+    elif user_input is '2':
+        query_block_model(blocks)
 
     user_input = get_user_input()
 
 print("Goodbye !")
-    
