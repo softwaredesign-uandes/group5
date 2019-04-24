24f170117a2092472d1d16879b966bc544e351ce
Martín Díaz <mfdiaz1@miuandes.cl>
2019-04-24 00:54:51 -0400

    Red phase: Added test for reblocking of empty model

--- BlockModelEditor/tests/test_block_model_editor.py
+++ BlockModelEditor/tests/test_block_model_editor.py
@@ -1,1 +1,8 @@
 import block_model_editor
+import block_model
+
+
+def test_model_reblock_empty_model():
+    empty_model = block_model.BlockModel()
+    reblocked_model = block_model_editor.reblock_model(empty_model, 4, 4, 4)
+    assert reblocked_model.get_total_block_number() == 0, "Total block number should be 0"
