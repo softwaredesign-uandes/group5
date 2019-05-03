6126c3e4611bf133dfc49c24f31a892fd4c65827
Martín Díaz <mfdiaz1@miuandes.cl>
2019-05-03 06:15:18 -0400

    Red phase: Added test for total number of blocks in reblocked model when model dimensions are not divisible by reblock factor

--- BlockModelEditor/tests/test_block_model_editor.py
+++ BlockModelEditor/tests/test_block_model_editor.py
@@ -30,6 +30,21 @@ def test_model_reblock_total_number():
     assert reblocked_model.get_total_block_number() == 1, "Total number should be 1"
 
 
+def test_model_reblock_not_divisible_by_factor_total_number():
+    block1 = block_model.Block(10.8, 5.3)
+    block2 = block_model.Block(20, 8)
+    block3 = block_model.Block(13.4, 3.7)
+    block4 = block_model.Block(8.7, 2.5)
+    blocks = block_model.BlockModel()
+    blocks.add_block((0, 0, 0), block1)
+    blocks.add_block((0, 0, 1), block2)
+    blocks.add_block((0, 1, 0), block3)
+    blocks.add_block((0, 1, 1), block4)
+
+    reblocked_model = block_model_editor.reblock_model(blocks, 2, 2, 2)
+    assert reblocked_model.get_total_block_number() == 1, "Total number should be 1"
+
+
 def test_model_reblock_weight():
     block1 = block_model.Block(10.8, 5.3)
     block2 = block_model.Block(20, 8)
